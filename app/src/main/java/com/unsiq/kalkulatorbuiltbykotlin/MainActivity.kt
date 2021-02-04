72890ce610470ec9659b9b6374c4fa6f38771b5c
Rijal Muhyidin <71188953+rijal404@users.noreply.github.com>
2021-02-04 12:21:49 +0700

    Pesan

---
+++ app/src/main/java/com/unsiq/kalkulatorbuiltbykotlin/MainActivity.kt
@@ -0,0 +1,81 @@
+package com.unsiq.kalkulatorbuiltbykotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import net.objecthunter.exp4j.ExpressionBuilder
+import java.lang.Exception
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //listeners untuk nomor
+        btn00.setOnClickListener { appendOnClick(true,"00") }
+        btn0.setOnClickListener { appendOnClick(true, "0") }
+        btn1.setOnClickListener { appendOnClick (true, "1") }
+        btn2.setOnClickListener { appendOnClick(true,"2") }
+        btn3.setOnClickListener { appendOnClick(true,"3") }
+        btn4.setOnClickListener { appendOnClick(true,"4") }
+        btn5.setOnClickListener { appendOnClick(true,"5") }
+        btn6.setOnClickListener { appendOnClick(true, "6") }
+        btn7.setOnClickListener { appendOnClick(true,"7") }
+        btn8.setOnClickListener { appendOnClick(true,"8") }
+        btn9.setOnClickListener { appendOnClick(true,"9") }
+        btnDot.setOnClickListener { appendOnClick(true,".") }
+
+        //listeners untuk operatpr
+        btnPlus.setOnClickListener { appendOnClick(false,"+") }
+        btnMinus.setOnClickListener { appendOnClick(false,"-") }
+        btnMultiply.setOnClickListener { appendOnClick(false,"*") }
+        btnDivide.setOnClickListener { appendOnClick(false,"/") }
+        btnLeftB.setOnClickListener { appendOnClick(false,"(") }
+        btnRightB.setOnClickListener { appendOnClick(false,")") }
+
+        btnClear.setOnClickListener {
+            clear()
+        }
+
+        btnEqual.setOnClickListener {
+            calculate()
+        }
+
+
+    }
+
+    private fun appendOnClick(clear: Boolean, string: String) {
+
+        if (clear){
+            tvOutput.text = ""
+            tvInput.append(string)
+        }else{
+            tvInput.append(tvOutput.text)
+            tvInput.append(string)
+            tvOutput.text = ""
+        }
+
+    }
+
+    private fun clear() {
+        tvInput.text = ""
+        tvOutput.text = ""
+    }
+
+    private fun calculate() {
+        try {
+
+            val input = ExpressionBuilder(tvInput.text.toString()).build()
+            val output = input.evaluate()
+            val longOutput = output.toLong()
+            if (output == longOutput.toDouble()) {
+                tvOutput.text = longOutput.toString()
+            }else{
+                tvOutput.text = output.toString()
+            }
+        }catch (e : Exception){
+            Toast.makeText(this@MainActivity,"Masukkan Operasi Yang Benar Bro!", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
